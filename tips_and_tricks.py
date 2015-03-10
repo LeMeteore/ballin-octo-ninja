ceee0b45924b39f94fdf1d6d3ea4bbba6e91b8ae
patrick nsukami <ndkpatt@gmail.com>
2015-03-10 23:01:49 +0000

    Tips & tricks: input validation using sets.

--- tips_and_tricks.py
+++ tips_and_tricks.py
@@ -5,3 +5,12 @@
 ss = ('pitt', 'patt', 'matt')
 for i, s in enumerate(ss):
     print(i, s)
+
+
+# input validation using sets
+vv = {'patt', 'matt', 'pitt', 'dan', 'din', 'don'}
+iv = {'patt', 'matt', 'pitt', 'rick'}
+
+print(iv.intersection(vv))
+print(iv.difference(vv))
+print(iv.issubset(vv))
